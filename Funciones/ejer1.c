84b52235e0d8298154b3b3d81bc82b2790dee93b
SantiagoMolinasoftware <146997008+SantiagoMolinasoftware@users.noreply.github.com>
2023-10-23 16:34:37 -0500

    Add files via upload

---
+++ Funciones/ejer1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int sumar(int a, int b)
+{
+    int operacion = a + b;
+}
+
+void main()
+{
+    int num1 = 0, num2 = 0;
+    int resultado;
+    printf("Ingrese el 1er valor: ");
+    scanf("%d", &num1);
+    printf("Ingrese el 2do valor: ");
+    scanf("%d", &num2);
+
+    resultado = sumar(num1, num2);
+
+    printf("El resultado es: %d", resultado);
+}
